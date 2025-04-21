b4bf1cb5b2d3d8fa9dcf02ecf004854c168e50c6
Tống Tùng Giang <tongtunggiang@gmail.com>
2025-04-21 18:22:17 +0100

    Add a few functions from Inigo: https://iquilezles.org/articles/functions/

--- BookOfShaders/PlotPS.hlsl
+++ BookOfShaders/PlotPS.hlsl
@@ -20,6 +20,28 @@ float Plot(float2 st, float pct){
           smoothstep( pct, pct+0.02, st.y);
 }
 
+// Some other functions
+// https://iquilezles.org/articles/functions/
+
+float ExpoImpulse( float x, float k )
+{
+    float h = k*x;
+    return h*exp(1.0-h);
+}
+float QuadImpulse( float k, float x )
+{
+    return 2.0*sqrt(k)*x/(1.0+k*x*x);
+}
+float Parabola( float x, float k )
+{
+    return pow( 4.0*x*(1.0-x), k );
+}
+float RationalBump( float x, float k )
+{
+    return 1.0/(1.0+k*x*x);
+}
+
+
 PSOutput MainPS(PSInput input)
 {
 	PSOutput ret = (PSOutput)0;
@@ -63,6 +85,27 @@ PSOutput MainPS(PSInput input)
 		case PlotMode::Floor:
 		{
 			y = floor(st.x);
+			break;
+		}
+		// Start of Inigo's functions
+		case PlotMode::ExponentialImpulse:
+		{
+			y = ExpoImpulse(st.x, /*$(Variable:Expo_Power)*/);
+			break;
+		}
+		case PlotMode::QuadraticImpulse:
+		{
+			y = QuadImpulse(/*$(Variable:Expo_Power)*/, st.x);
+			break;
+		}
+		case PlotMode::Parabola:
+		{
+			y = Parabola(st.x, /*$(Variable:Expo_Power)*/);
+			break;
+		}
+		case PlotMode::RationalBump:
+		{
+			y = RationalBump(st.x, /*$(Variable:Expo_Power)*/);
 			break;
 		}
 		default:
